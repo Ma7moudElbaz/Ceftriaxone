75694cf4a32884a58cb0611a166e8478383677d3
Mahmoud Elbaz <dev.mahmoud.elbaz@gmail.com>
2020-07-11 16:25:12 +0400

    11/07/2020 commit

--- app/src/main/java/com/cat/ceftriaxone/indications/Indications_fragment.java
+++ app/src/main/java/com/cat/ceftriaxone/indications/Indications_fragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,9 +15,20 @@ import android.view.ViewGroup;
 
 import com.cat.ceftriaxone.MainActivity;
 import com.cat.ceftriaxone.R;
+import com.cat.ceftriaxone.speciality.Indications_names;
+import com.cat.ceftriaxone.speciality.speciality_indications.Indications_adapter;
+import com.cat.ceftriaxone.speciality.speciality_indications.Indications_item;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 
 public class Indications_fragment extends Fragment {
+
+
+    RecyclerView indicationsRecycler;
+    ArrayList<Indications_item> items;
+    Indications_adapter adapter;
 
 
     @Override
@@ -30,5 +44,43 @@ public class Indications_fragment extends Fragment {
         MainActivity activity = (MainActivity) getActivity();
         activity.setIndicationsColors();
 
+        items = new ArrayList<>();
+        setIndicationsItems();
+
+        indicationsRecycler = view.findViewById(R.id.indications_recycler);
+        setIndicationsRecycler();
+
+    }
+
+
+    private void setIndicationsItems() {
+
+        Indications_names names = new Indications_names();
+        Map<Integer, String> indicationsNames = names.getNames();
+
+        items.add(new Indications_item(3, R.drawable.ic_chest, indicationsNames.get(1)));
+        items.add(new Indications_item(4, R.drawable.ic_ent, indicationsNames.get(2)));
+        items.add(new Indications_item(9, R.drawable.ic_im_gp, indicationsNames.get(3)));
+        items.add(new Indications_item(10, R.drawable.ic_im_gp, indicationsNames.get(3)));
+        items.add(new Indications_item(1, R.drawable.ic_chest, indicationsNames.get(3)));
+        items.add(new Indications_item(13, R.drawable.ic_urology, indicationsNames.get(3)));
+        items.add(new Indications_item(5, R.drawable.ic_surgeon, indicationsNames.get(3)));
+        items.add(new Indications_item(12, R.drawable.ic_im_gp, indicationsNames.get(3)));
+        items.add(new Indications_item(14, R.drawable.ic_urology, indicationsNames.get(3)));
+        items.add(new Indications_item(2, R.drawable.ic_chest, indicationsNames.get(3)));
+        items.add(new Indications_item(8, R.drawable.ic_im_gp, indicationsNames.get(3)));
+        items.add(new Indications_item(7, R.drawable.ic_im_gp, indicationsNames.get(3)));
+        items.add(new Indications_item(11, R.drawable.ic_im_gp, indicationsNames.get(3)));
+        items.add(new Indications_item(6, R.drawable.ic_surgeon, indicationsNames.get(3)));
+        items.add(new Indications_item(15, R.drawable.ic_urology, indicationsNames.get(3)));
+
+    }
+
+
+    private void setIndicationsRecycler() {
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL);
+        adapter = new Indications_adapter(getActivity(), items);
+        indicationsRecycler.setLayoutManager(layoutManager);
+        indicationsRecycler.setAdapter(adapter);
     }
 }
