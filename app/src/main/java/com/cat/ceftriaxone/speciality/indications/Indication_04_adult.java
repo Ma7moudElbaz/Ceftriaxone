01bffbb8f9fb15d547f9cea49da92dfd4b5b8c85
Mahmoud Elbaz <dev.mahmoud.elbaz@gmail.com>
2020-07-11 19:53:58 +0400

    11/07/2020 commit

--- app/src/main/java/com/cat/ceftriaxone/speciality/indications/Indication_04_adult.java
+++ app/src/main/java/com/cat/ceftriaxone/speciality/indications/Indication_04_adult.java
@@ -1,5 +1,7 @@
 package com.cat.ceftriaxone.speciality.indications;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,6 +11,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.cat.ceftriaxone.R;
 
@@ -21,8 +24,19 @@ public class Indication_04_adult extends Fragment {
         return inflater.inflate(R.layout.fragment_indication_04_adult, container, false);
     }
 
+    ImageView info;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+         info = view.findViewById(R.id.info);
+         info.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 new AlertDialog.Builder(getActivity())
+                    .setMessage("Limited data suggest that If the patient is severely ill or the previous therapy has failed, Ceftriaxone may be effective when given for 3 days.")
+
+                    .show();
+             }
+         });
     }
 }
