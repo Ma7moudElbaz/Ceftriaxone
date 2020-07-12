54c22ddf8db1a47331a253eb5edd1f439add4721
Mahmoud Elbaz <dev.mahmoud.elbaz@gmail.com>
2020-07-12 17:46:15 +0400

    12/07/2020 commit

--- app/src/main/java/com/cat/ceftriaxone/speciality/indications/Indication_09_ped.java
+++ app/src/main/java/com/cat/ceftriaxone/speciality/indications/Indication_09_ped.java
@@ -9,6 +9,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cat.ceftriaxone.R;
 
@@ -23,8 +28,62 @@ public class Indication_09_ped extends Fragment {
         return inflater.inflate(R.layout.fragment_indication_09_ped, container, false);
     }
 
+    ImageView next, prev;
+    ScrollView slide1, slide2;
+    EditText dosage, weight;
+    TextView indicationDosage, dose;
+
+    int minDosage, maxDosage;
+    String dosageTxt;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        next = view.findViewById(R.id.next);
+        prev = view.findViewById(R.id.prev);
+        slide1 = view.findViewById(R.id.slide_1);
+        slide2 = view.findViewById(R.id.slide_2);
+        indicationDosage = view.findViewById(R.id.indication_dosage);
+        dosage = view.findViewById(R.id.et_dosage);
+        weight = view.findViewById(R.id.et_weight);
+        dose = view.findViewById(R.id.dose);
+
+        minDosage = 80;
+        maxDosage = 100;
+        dosageTxt = minDosage + "-" + maxDosage;
+        indicationDosage.setText(dosageTxt);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateDose();
+            }
+        });
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                slide1.setVisibility(View.VISIBLE);
+                slide2.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void calculateDose() {
+        String currentDosage = dosage.getText().toString();
+        String currentWeight = weight.getText().toString();
+
+        if (currentDosage.length() == 0 || currentWeight.length() == 0) {
+            Toast.makeText(getActivity(), "Please fill all fields", Toast.LENGTH_SHORT).show();
+        } else if (Integer.parseInt(currentDosage) < minDosage || Integer.parseInt(currentDosage) > maxDosage) {
+            Toast.makeText(getActivity(), "Dosage should Be "+dosageTxt, Toast.LENGTH_SHORT).show();
+
+        } else {
+            int totalDose = Integer.parseInt(currentDosage) * Integer.parseInt(currentWeight);
+            dose.setText(String.valueOf(totalDose));
+            slide1.setVisibility(View.GONE);
+            slide2.setVisibility(View.VISIBLE);
+        }
     }
 }
