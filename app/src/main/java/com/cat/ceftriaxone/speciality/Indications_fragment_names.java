d11b6a08aa099e517170738bf4b1bccaa21e9d48
Mahmoud Elbaz <dev.mahmoud.elbaz@gmail.com>
2020-07-12 12:27:07 +0400

    12/07/2020 commit

--- app/src/main/java/com/cat/ceftriaxone/speciality/Indications_fragment_names.java
+++ app/src/main/java/com/cat/ceftriaxone/speciality/Indications_fragment_names.java
@@ -2,10 +2,21 @@ package com.cat.ceftriaxone.speciality;
 
 import androidx.fragment.app.Fragment;
 
+import com.cat.ceftriaxone.speciality.indications.Indication_02_ped;
+import com.cat.ceftriaxone.speciality.indications.Indication_04_ped;
+import com.cat.ceftriaxone.speciality.indications.Indication_05_ped;
+import com.cat.ceftriaxone.speciality.indications.Indication_06_ped;
+import com.cat.ceftriaxone.speciality.indications.Indication_07_ped;
+import com.cat.ceftriaxone.speciality.indications.Indication_08_ped;
+import com.cat.ceftriaxone.speciality.indications.Indication_09_ped;
 import com.cat.ceftriaxone.speciality.indications.Indication_10_adult;
+import com.cat.ceftriaxone.speciality.indications.Indication_10_ped;
 import com.cat.ceftriaxone.speciality.indications.Indication_11_adult;
+import com.cat.ceftriaxone.speciality.indications.Indication_11_ped;
 import com.cat.ceftriaxone.speciality.indications.Indication_12_adult;
+import com.cat.ceftriaxone.speciality.indications.Indication_12_ped;
 import com.cat.ceftriaxone.speciality.indications.Indication_13_adult;
+import com.cat.ceftriaxone.speciality.indications.Indication_13_ped;
 import com.cat.ceftriaxone.speciality.indications.Indication_14_adult;
 import com.cat.ceftriaxone.speciality.indications.Indication_15_adult;
 import com.cat.ceftriaxone.speciality.indications.Indication_01_adult;
@@ -18,6 +29,7 @@ import com.cat.ceftriaxone.speciality.indications.Indication_06_adult;
 import com.cat.ceftriaxone.speciality.indications.Indication_07_adult;
 import com.cat.ceftriaxone.speciality.indications.Indication_08_adult;
 import com.cat.ceftriaxone.speciality.indications.Indication_09_adult;
+import com.cat.ceftriaxone.speciality.indications.Indication_15_ped;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -53,20 +65,20 @@ public class Indications_fragment_names {
 
     private void setFragmentsNames_ped(){
         fragmentsNames_ped.put(1, new Indication_01_ped());
-        fragmentsNames_ped.put(2, new Indication_02_adult());
+        fragmentsNames_ped.put(2, new Indication_02_ped());
         fragmentsNames_ped.put(3, new Indication_03_adult());
-        fragmentsNames_ped.put(4, new Indication_04_adult());
-        fragmentsNames_ped.put(5, new Indication_05_adult());
-        fragmentsNames_ped.put(6, new Indication_06_adult());
-        fragmentsNames_ped.put(7, new Indication_07_adult());
-        fragmentsNames_ped.put(8, new Indication_08_adult());
-        fragmentsNames_ped.put(9, new Indication_09_adult());
-        fragmentsNames_ped.put(10, new Indication_10_adult());
-        fragmentsNames_ped.put(11, new Indication_11_adult());
-        fragmentsNames_ped.put(12, new Indication_12_adult());
-        fragmentsNames_ped.put(13, new Indication_13_adult());
+        fragmentsNames_ped.put(4, new Indication_04_ped());
+        fragmentsNames_ped.put(5, new Indication_05_ped());
+        fragmentsNames_ped.put(6, new Indication_06_ped());
+        fragmentsNames_ped.put(7, new Indication_07_ped());
+        fragmentsNames_ped.put(8, new Indication_08_ped());
+        fragmentsNames_ped.put(9, new Indication_09_ped());
+        fragmentsNames_ped.put(10, new Indication_10_ped());
+        fragmentsNames_ped.put(11, new Indication_11_ped());
+        fragmentsNames_ped.put(12, new Indication_12_ped());
+        fragmentsNames_ped.put(13, new Indication_13_ped());
         fragmentsNames_ped.put(14, new Indication_14_adult());
-        fragmentsNames_ped.put(15, new Indication_15_adult());
+        fragmentsNames_ped.put(15, new Indication_15_ped());
     }
 
     public Map<Integer, Fragment> getFragmentsNames_adult() {
---
+++ app/src/main/java/com/cat/ceftriaxone/speciality/indications/Indication_15_ped.java
@@ -0,0 +1,29 @@
+package com.cat.ceftriaxone.speciality.indications;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cat.ceftriaxone.R;
+
+
+public class Indication_15_ped extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_indication_15_ped, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
